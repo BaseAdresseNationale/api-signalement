cc6cdda2e08f9221355d61fa794b478f381a8b48
Guillaume Fay <guillaume_fay@hotmail.com>
2025-04-16 09:32:49 -0600

    feat: COG 2025

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,3 +1,7 @@
 import { PostGisAddSignalementPoint1741898533813 } from './1741898533813-postgis-add-signalement-point';
+import { COG2025_1744813616445 } from './1744813616445-cog-2025';
 
-export const migrations = [PostGisAddSignalementPoint1741898533813];
+export const migrations = [
+  PostGisAddSignalementPoint1741898533813,
+  COG2025_1744813616445,
+];
