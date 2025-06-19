7f00f338daa174041409d2ecf7c0456ae6f61060
Guillaume Fay <guillaume_fay@hotmail.com>
2025-06-19 11:47:52 +0200

    feat: COG 2025 (#22)

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,11 @@
 import { PostGisAddSignalementPoint1741898533813 } from './1741898533813-postgis-add-signalement-point';
 import { AddRejectionReason1746196994065 } from './1746196994065-add-rejection-reason';
 import { AddSettings1747315664342 } from './1747315664342-add-settings';
+import { COG2025_1750325214122 } from './1750325214122-cog-2025';
 
 export const migrations = [
   PostGisAddSignalementPoint1741898533813,
   AddRejectionReason1746196994065,
   AddSettings1747315664342,
+  COG2025_1750325214122,
 ];
