7bf2dbf41f73433aabf666b29c37b73c5cee8f28
Guillaume Fay <guillaume_fay@hotmail.com>
2025-07-31 17:50:56 +0200

    feat: add toponyme in location to create

--- src/modules/signalement/dto/signalement.dto.ts
+++ src/modules/signalement/dto/signalement.dto.ts
@@ -30,6 +30,7 @@ import {
   SignalementTypeEnum,
 } from '../signalement.types';
 import { Signalement } from '../signalement.entity';
+import { isToponymeChangesRequested } from '../signalement.utils';
 
 export class CreateSignalementInput {
   @ApiProperty({ required: true, nullable: false, type: String })
@@ -122,6 +123,9 @@ export class CreateSignalementInput {
       case SignalementTypeEnum.LOCATION_TO_DELETE:
         return DeleteNumeroChangesRequestedDTO;
       case SignalementTypeEnum.LOCATION_TO_CREATE:
+        if (isToponymeChangesRequested(payload.changesRequested)) {
+          return ToponymeChangesRequestedDTO;
+        }
         return NumeroChangesRequestedDTO;
       default:
         throw new Error('Invalid signalement type');
