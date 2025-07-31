7c6973e7baf27cb3a85b181e6c73e1a6618077f9
Guillaume Fay <guillaume_fay@hotmail.com>
2025-07-31 11:16:21 +0200

    fix: update migration

--- src/migrations/1753283848477-fix-numero-string-in-changes-requested.ts
+++ src/migrations/1753283848477-fix-numero-string-in-changes-requested.ts
@@ -28,16 +28,14 @@ export class FixNumeroStringInChangesRequested1753283848477
         .map(({ id, changesRequested }: { id: string; changesRequested }) => {
           const { numero } = changesRequested as NumeroChangesRequestedDTO;
 
-          if (numero && typeof numero === 'string') {
-            console.log(
-              `Signalement ${id} has a string numero, converting to number : ${numero}`,
-            );
+          console.log(
+            `Signalement ${id} has a string numero, converting to number : ${numero}`,
+          );
 
-            return queryRunner.query(
-              `UPDATE "signalements" SET "changes_requested" = jsonb_set("changes_requested", '{numero}', $1) WHERE id = $2`,
-              [parseInt(numero, 10), id],
-            );
-          }
+          return queryRunner.query(
+            `UPDATE "signalements" SET "changes_requested" = jsonb_set("changes_requested", '{numero}', $1) WHERE id = $2`,
+            [parseInt(numero as unknown as string, 10), id],
+          );
         }),
     );
   }
