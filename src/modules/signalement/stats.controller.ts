f705ae8b46e2323b1052a06d0f43c97d72430822
Guillaume Fay <guillaume_fay@hotmail.com>
2025-09-05 13:33:59 +0200

    fix: remove auth for stats endpoint

--- src/modules/signalement/stats.controller.ts
+++ src/modules/signalement/stats.controller.ts
@@ -1,14 +1,8 @@
-import { Controller, Get, HttpStatus, Res, UseGuards } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Res } from '@nestjs/common';
 import { Response } from 'express';
 import { SignalementService } from './signalement.service';
-import {
-  ApiBearerAuth,
-  ApiOperation,
-  ApiResponse,
-  ApiTags,
-} from '@nestjs/swagger';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { SignalementStatsDTO } from './dto/stats.dto';
-import { AdminGuard } from '../../common/admin.guard';
 
 @Controller('stats')
 @ApiTags('stats')
@@ -24,8 +18,6 @@ export class StatsController {
     status: HttpStatus.OK,
     type: SignalementStatsDTO,
   })
-  @ApiBearerAuth('admin-token')
-  @UseGuards(AdminGuard)
   async getStats(@Res() res: Response) {
     const stats = await this.signalementService.getStats();
     res.status(HttpStatus.OK).json(stats);
