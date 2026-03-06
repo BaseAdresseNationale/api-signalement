33557ce264caa35a4673a336d0c611764b0c18a9
Guillaume Fay <guillaume_fay@hotmail.com>
2026-03-06 20:52:34 +0100

    fix: lint

--- src/common/base-report.service.ts
+++ src/common/base-report.service.ts
@@ -47,6 +47,7 @@ export abstract class BaseReportService<
 
   /** Retourne les champs supplémentaires à passer au `repository.update()` */
   protected getExtraUpdateFields(
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     _updateDTO: UpdateReportDTO,
   ): Partial<Record<string, any>> {
     return {};
