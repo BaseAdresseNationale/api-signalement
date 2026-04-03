ccf5638381b2761ae26fd7db4b3568dfa4a1ee24
Guillaume Fay <guillaume_fay@hotmail.com>
2026-04-03 16:54:16 +0200

    fix: cors enabled and remove cookie parser

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: process.env.MES_SIGNALEMENTS_URL,
-    credentials: true,
-  });
-  app.use(cookieParser());
+  app.enableCors();
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
