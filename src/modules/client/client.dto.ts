257d5e6819ce92adfdff24b59f550df4452ef397
Guillaume Fay <guillaume_fay@hotmail.com>
2026-09-03 17:43:39 +0200

    fix: tests

--- src/modules/client/client.dto.ts
+++ src/modules/client/client.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class CreateClientDTO {
   @ApiProperty({ required: true, nullable: false })
@@ -8,6 +8,7 @@ export class CreateClientDTO {
   nom: string;
 
   @ApiProperty({ required: false, nullable: true })
+  @IsOptional()
   @IsString()
   partenaireId?: string;
 }
